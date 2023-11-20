1294683b6ebe0c15b10ea310a3ec6e16a690aeec
Eitanmohorade <eitanluc@gmail.com>
2023-11-20 17:34:32 -0300

    travel data functionalities

--- dsd8y9/Include/app.py
+++ dsd8y9/Include/app.py
@@ -1,8 +1,9 @@
 import pandas as pd
-import graficos as func
+import graficos
+import travel_data as email
 
 
-
+path_txt = 'C:/Users/Lochofo/Desktop/PAGINAS/Python_Intensive/dsd8y9/Include/email.txt'
 path = "C:/Users/Lochofo/Desktop/PAGINAS/Python_Intensive/dsd8y9/Include/integrados_8_y_9.xlsx"
 respuestas = pd.read_excel(path)
 pd.set_option('display.max_rows', None)
@@ -11,6 +12,11 @@ pd.set_option('display.max_columns', None)
 arboles_xcuadra = respuestas["Cuantos arboles hay en su cuadra? (aprox)"].value_counts()
 tachos_xcuadra = respuestas["Cuantos tachos de basura hay en su cuadra?"]
 
-func.grafico_barra(arboles_xcuadra, "Respuestas", "Frecuencia", "Porcentaje de respuestas sobre arboles por cuadra", 'm')
+#graficos.grafico_barra(arboles_xcuadra, "Respuestas", "Frecuencia", "Porcentaje de respuestas sobre arboles por cuadra", 'm')
 
-func.grafico_comun(tachos_xcuadra, "Respuesta", "Frecuencia", "Cantidad de tachos de basura por cuadra")
+#graficos.grafico_comun(tachos_xcuadra, "Respuesta", "Frecuencia", "Cantidad de tachos de basura por cuadra")
+
+datos = email.usuario_contrasenia(path_txt)
+print(datos)
+
+#email.enviar_mail()
--- dsd8y9/Include/travel_data.py
+++ dsd8y9/Include/travel_data.py
@@ -1,13 +1,29 @@
 import yagmail
 
-email = ""
-password = ""
+'''
+    devolverá
+    dos variables: mail
+    y contraseña.
+'''
+#puse contrasenia sin enie por las dudas
+def usuario_contrasenia(ruta):
+    datos = []
+    archivo = open(ruta, 'r')
+    datos.append(archivo.read())
+    archivo.close()
+    return datos
 
-yag = yagmail.SMTP(user=email, password=password)
-destino = ['']
-
-asunto = ''
-msj = ""
-titulo = ""
-
-yag.send(destino, asunto, [titulo, msj])
+'''
+    Una función que se llame: enviar_mail. Esta función 
+    utiliza la librería yagmail.
+    Recibirá los datos: mail, contraseña, asunto,
+    mensaje, destinatarios. La función
+    utilizará mail y contraseña para enviar un mail, 
+    para el asunto utilizará la variable
+    asunto y para el cuerpo del mail, mensaje. El mail lo 
+    enviará a la lista de destinatario
+    en destinatarios.
+'''
+def enviar_mail(mail, contrasenia, asunto, mensaje, destinatarios):
+    yag = yagmail.SMTP(user=mail, password=contrasenia)
+    return yag.send(destinatarios, asunto, mensaje)
